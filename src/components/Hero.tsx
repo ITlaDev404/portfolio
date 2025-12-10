0b6c9bc1c6ce7e19d448263b5f86e50047083acf
Tlati <ilyesstlati92@egmail.com>
2025-12-10 13:04:57 +0100

    add name

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <section className="bg-blue-600 text-white p-16 text-center min-h-64 flex items-center justify-center">
-      <h1 className="text-5xl font-extrabold">Hero Component</h1>
+      <h1 className="text-5xl font-extrabold">Tlati Ilyes </h1>
     </section>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'; // Import the tailwindcss plugin
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Set the base path for GitHub Pages
   plugins: [react(), tailwindcss()], // Add the tailwindcss plugin here
 })
