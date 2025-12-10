94f9fc274c1b60e8971fe40cf69422002c5c1a4a
Tlati <ilyesstlati92@egmail.com>
2025-12-10 13:06:21 +0100

    add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4 text-center text-2xl font-bold">
-      <h1>Header Component</h1>
+      <h1>Portfolio</h1>
     </header>
   );
 };
