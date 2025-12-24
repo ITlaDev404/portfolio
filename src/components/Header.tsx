cdc190f84f9cb3c2137ce85dde2d1568f5845d8e
Tlati <ilyesstlati92@egmail.com>
2025-12-24 13:13:48 +0100

    finir Bouton mon cv

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
 const Header = () => {
   return (
     <header className="bg-componentColor text-protfolioText p-4 text-center text-2xl font-bold shadow-lg">
-      <h1>Portfolio</h1>
+      <button className="bg-componentColor2 hover:bg-componentColor3 active:bg-componentColor border-solid border-borderColor text-portfolioText text-center bg-center text-2xl  grid-cols-1 mr-4 px-4 py-2 rounded  ">
+        Navigation{" "}
+      </button>
+      <button
+        onClick={() => window.open("https://drive.google.com/file/d/1HUvkICiB2hwOcd40q_M3aKo1G25wBHAq/view?usp=drive_link", "_blank", "noopener,noreferrer")
+        }
+        className="bg-componentColor2 hover:bg-componentColor3 active:bg-componentColor border-solid border-borderColor text-portfolioText text-center bg-center text-2xl  grid-cols-1 px-4 py-2 rounded  "
+      >
+        Télécharger CV
+      </button>
+      <button className="bg-componentColor2 hover:bg-componentColor3 active:bg-componentColor border-solid border-borderColor text-portfolioText text-center bg-center text-2xl  grid-cols-1 ml-4 px-4 py-2 rounded ">
+        Contact
+      </button>
     </header>
   );
 };
