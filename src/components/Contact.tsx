e3ba54d2a973eb2e2b3641638f631904a83717c4
Tlati <ilyesstlati92@egmail.com>
2026-01-09 14:47:07 +0100

    spelling mistakes corrected  with IA

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ const Contact = () => {
       className="bg-protfolioText text-black p-12 text-center"
     >
       <h1 className="text-4xl font-bold mb-4">Contact</h1>
-      <p className="text-lg">N'hésitez pas à me contacter !</p>
+      <p className="text-lg">N'hésitez pas à me contacter ! </p>
     </section>
   );
 };
