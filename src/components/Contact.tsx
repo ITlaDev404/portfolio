87fca8a5456726ff2ff22ab560af5de1116ff32a
Tlati <ilyesstlati92@egmail.com>
2026-02-05 10:55:53 +0100

    change E5.pdf

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,10 +5,9 @@ const Contact = () => {
       className="bg-protfolioText text-black p-12 text-center"
     >
       <h1 className="text-4xl font-bold mb-4">Contact</h1>
-      <p className="text-lg">N'hésitez pas à me contacter ! </p>
+      <p className="text-lg">N'hésitez pas à me contacter !</p>
     </section>
   );
 };
-
 
 export default Contact;
