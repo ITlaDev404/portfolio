13c5dcf683332325dad5ddb4860246a076319a39
Tlati <ilyesstlati92@egmail.com>
2026-08-01 09:36:24 +0200

    update the About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,15 +5,16 @@ const About = () => {
       className="  bg-componentColor  text-portfolioText p-12 p-max-12 text-center hover:shadow-lg max-w-prose mx-auto rounded-xl mb-12 border-borderColor border hover:border-1"
     >
       <h1 className="text-4xl font-bold mb-4">Présentation</h1>
-      <p className="text-lg">Bonjour, c'est Ilyes Tlati 👋 </p>
+      <p className="text-lg">Bonjour, c'est Ilyes Tlati 👋</p>
       <p className=" #about text-lg text-center  max-w-prose  mx-auto">
-        Je suis actuellement étudiant au lycée Turgot à Paris 3e 🗺️, en deuxième
-        année de BTS SIO (Services Informatiques aux Organisations), option SLAM
-        (Solutions Logicielles et Applications Métiers), une spécialisation dans
-        le développement 💻. Toujours en cours d'études 🎓, je vous présente sur
-        ce portfolio les différentes études et projets professionnels que j'ai
-        pu entreprendre. Vous retrouverez mon CV ci-dessus et mes différents
-        projets ci-dessous 🧑‍💻
+        Étudiant en BTS SIO option SLAM 🎓, admis en Licence Informatique L3 au
+        CNAM Paris pour la rentrée 2026, je recherche une alternance en
+        développement logiciel, web ou systèmes 💻. Expérience en développement
+        d'applications web, bases de données, environnements Linux 🐧 ainsi
+        qu'en maintenance et administration de réseaux (VLAN, DHCP, DNS) 🖥️.
+        Motivé par la conception de solutions techniques et le travail en
+        équipe 🤝. Vous retrouverez mon CV ci-dessus et mes différents projets
+        ci-dessous 🧑‍💻
       </p>
     </section>
   );
